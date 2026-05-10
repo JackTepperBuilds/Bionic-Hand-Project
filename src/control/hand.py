09eb07e477edb746fa2c18105948a59b3ad7724e
Jack Tepper <jatep2005@gmail.com>
2026-05-10 19:33:48 -0400

    refactor: created a method with a nested loop for individual finger control instead of previous code containing separate gesture functions

--- src/control/hand.py
+++ src/control/hand.py
@@ -7,11 +7,12 @@ import adafruit_pca9685 # Servo driver that contains I2C
 # Sets up i2c communication and passes it into the sevo driver (pca) and current_gesture
 # tracks what the current gesture being run is.
 class Hand:
-    def __init__(self, pca, prev_gesture, fingers, finger_list):
+    def __init__(self, pca, prev_gesture, fingers, finger_list, current_gesture):
         i2c: busio.I2C = busio.I2C(board.SCL, board.SDA)
         self.pca = adafruit_pca9685(i2c)
         self.pca.frequency = 50
         self.prev_gesture = 'open'
+        self.current_gesture = 'open'
 
         # finger_list dictionary that contains every gesture and their respective list of what 
         # fingers whould be up (1) and what fingers should be down (0).
@@ -33,11 +34,31 @@ class Hand:
 
     # A method containing a nested for loop with an outer loop that contains a 5 finger array 
     # of the last gesture and an inner loop of a 5 finger array of the gesture I am trying to perform.
-
-    # TODO: compare the previous gesture tuple to current gesture tuple. When there is a difference 
-    # convert the tuple numbers to a list of the finger names using x = fingers.keys. Then using the index 
-    # where the two tuples were different loop through the finger name list and get the respective
-    # finger name that needs to move. Then using a conditional, check if it needs to open (1) or
-    # close (0).
-    def run_hand(self, pca: adafruit_pca9685, prev_gesture: str, fingers: dict) -> None:
+    def run_hand(self, pca: adafruit_pca9685, prev_gesture: str, fingers: dict, finger_list: dict, 
+                 current_gesture: str) -> None:
         
+        # Contains lists of prev_gesture and current_gesture 1's and 0's.
+        prev_tuple: list[int] = finger_list[prev_gesture]
+        current_tuple: list[int] = finger_list[current_gesture]
+
+        # Contains a list of finger names based on their respective indexes.
+        finger_names: list[str] = fingers.keys
+
+        for x in range(5):
+            for y in range(5):
+                if prev_tuple[x] == current_tuple[y]:
+                    continue
+                elif prev_tuple[x] != current_tuple[y]:
+                    finger_name: str = finger_names[y]
+
+                    if y == 0:
+                        for j in range(180, -2, -2):
+                            fingers[finger_name].angle = j
+                            time.sleep(0.02)
+
+                    elif y == 1:
+                        for m in range(0, 182, 2):
+                            fingers[finger_name].angle = m
+                            time.sleep(0.02)
+
+                    prev_tuple: list[int] = current_tuple: list[int]
