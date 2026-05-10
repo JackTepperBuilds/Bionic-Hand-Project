c2f8ce2cb6e8abb796b5ec93b3428c3216b21574
Jack Tepper <jatep2005@gmail.com>
2026-05-10 01:14:40 -0400

    refactor: set the fingers dictionary into constructor

--- src/control/hand.py
+++ src/control/hand.py
@@ -7,10 +7,17 @@ import adafruit_pca9685 # Servo driver that contains I2C
 # Sets up i2c communication and passes it into the sevo driver (pca) and current_gesture
 # tracks what the current gesture being run is.
 class Hand:
-    def __init__(self, pca, prev_gesture):
+    def __init__(self, pca, prev_gesture, fingers):
         i2c: busio.I2C = busio.I2C(board.SCL, board.SDA)
         self.pca = adafruit_pca9685(i2c)
+        self.pca.frequency = 50
         self.prev_gesture = 'open'
 
-    # Dont forget frequency. May need to put that into the constructor.
-    def open_hand(self, pca, prev_gesture):
+        self.fingers: dict = {"pointer": servo.Servo(pca.channels[0], minpulse = 500, maxpulse = 2500), 
+                        "middle": servo.Servo(pca.channels[1], minpulse = 500, maxpulse = 2500),
+                        "ring": servo.Servo(pca.channels[2], minpulse = 500, maxpulse = 2500),
+                        "pinky": servo.Servo(pca.channels[3], minpulse = 500, maxpulse = 2500),
+                        "thumb": servo.Servo(pca.channels[4], minpulse = 500, maxpulse = 2500)}
+
+    def open_hand(self, pca: adafruit_pca9685, prev_gesture: str) -> None:
+        
