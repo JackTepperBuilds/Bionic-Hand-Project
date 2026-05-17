6385587a192be5c68c89342bf0bcba418c5a6a9f
Jack Tepper <jatep2005@gmail.com>
2026-05-16 20:19:49 -0400

    Fix: changed main object and method arguments

--- src/control/main.py
+++ src/control/main.py
@@ -4,18 +4,18 @@ print("Closed_Fist, Open_Palm, Victory, Thumb_Up, Pointing_Up, ILoveYou\n")
 x = input()
 
 prev_gesture = "none"
-gesture = Hand(prev_gesture, x)
+gesture = Hand()
 prev_gesture = x
 
 if x == "Closed_Fist":
-    gesture.run_hand()
+    gesture.run_hand(x)
 elif x == "Open_Palm":
-    gesture.run_hand()
+    gesture.run_hand(x)
 elif x == "Victory":
-    gesture.run_hand()
+    gesture.run_hand(x)
 elif x == "Thumb_Up":
-    gesture.run_hand()
+    gesture.run_hand(x)
 elif x == "Pointing_Up":
-    gesture.run_hand()
+    gesture.run_hand(x)
 elif x == "ILoveYou":
-    gesture.run_hand()
+    gesture.run_hand(x)
