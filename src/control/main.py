bc9d5f64f9a0254e7f8f69f8235e702f109cffa8
Jack Tepper <jatep2005@gmail.com>
2026-05-17 20:24:02 -0400

    Fix: looped the gesture control in main

--- src/control/main.py
+++ src/control/main.py
@@ -1,21 +1,12 @@
 from hand import Hand
 
-print("Closed_Fist, Open_Palm, Victory, Thumb_Up, Pointing_Up, ILoveYou\n")
-x = input()
+print("Closed_Fist, Open_Palm, Victory, Thumb_Up, Pointing_Up, ILoveYou, exit\n")
 
-prev_gesture = "none"
 gesture = Hand()
-prev_gesture = x
 
-if x == "Closed_Fist":
+while True:
+    x = input()
     gesture.run_hand(x)
-elif x == "Open_Palm":
-    gesture.run_hand(x)
-elif x == "Victory":
-    gesture.run_hand(x)
-elif x == "Thumb_Up":
-    gesture.run_hand(x)
-elif x == "Pointing_Up":
-    gesture.run_hand(x)
-elif x == "ILoveYou":
-    gesture.run_hand(x)
+
+    if x == "exit":
+        break
