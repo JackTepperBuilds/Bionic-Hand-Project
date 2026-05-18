dceb8b9b2aaf4e6968518d041800c949eca30ecd
Jack Tepper <jatep2005@gmail.com>
2026-05-18 00:15:44 -0400

    Fix: changed channels

--- src/control/hand.py
+++ src/control/hand.py
@@ -28,8 +28,8 @@ class Hand:
         # max and min pulses.
         self.fingers: dict = {"pointer": servo.Servo(pca.channels[0], min_pulse = 500, max_pulse = 2500), 
                         "middle": servo.Servo(pca.channels[1], min_pulse = 500, max_pulse = 2500),
-                        "ring": servo.Servo(pca.channels[3], min_pulse = 500, max_pulse = 2500),
-                        "pinky": servo.Servo(pca.channels[4], min_pulse = 500, max_pulse = 2500),
+                        "ring": servo.Servo(pca.channels[4], min_pulse = 500, max_pulse = 2500),
+                        "pinky": servo.Servo(pca.channels[3], min_pulse = 500, max_pulse = 2500),
                         "thumb": servo.Servo(pca.channels[2], min_pulse = 500, max_pulse = 2500)}
 
 
