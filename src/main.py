c5a8cd69ea18c20bc23d0ce13bf45a7e18bf2a1a
Jack Tepper <jatep2005@gmail.com>
2026-05-19 21:26:05 -0400

    style: moved the print to after the camera activates

--- src/main.py
+++ src/main.py
@@ -1,14 +1,13 @@
 from hand import Hand
 from vision import Vision
 
-print("Closed_Fist, Open_Palm, Victory, Thumb_Up, Pointing_Up, ILoveYou, exit\n")
-
 gesture = Hand()
 camera = Vision()
+camera.generator()
+
+print("Closed_Fist, Open_Palm, Victory, Thumb_Up, Pointing_Up, ILoveYou, exit\n")
 
 while True:
-    camera.generator()
-
     x = input()
     if x == "exit":
         break
