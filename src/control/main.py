8676a05c8950fd62a0a8287426533c71987c855f
Jack Tepper <jatep2005@gmail.com>
2026-05-19 20:22:15 -0400

    Feat: created camera object in main

--- src/control/main.py
+++ src/control/main.py
@@ -1,8 +1,10 @@
 from hand import Hand
+from vision import Vision
 
 print("Closed_Fist, Open_Palm, Victory, Thumb_Up, Pointing_Up, ILoveYou, exit\n")
 
 gesture = Hand()
+camera = Vision()
 
 while True:
     x = input()
