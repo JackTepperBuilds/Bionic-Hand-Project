be61b8cc89594d1a96f60c92e55d8f4be6930ccf
Jack Tepper <jatep2005@gmail.com>
2026-05-29 16:42:19 -0400

    fix: changed .sleep times

--- src/wrist_test.py
+++ src/wrist_test.py
@@ -30,7 +30,7 @@ while True:
         elif (current_gesture == 1 or current_gesture == 2):
             for i in range(180, 0, -1):
                 wrist.angle = i
-                time.sleep(0.02)
+                time.sleep(0.01)
 
         current_gesture = 0
 
@@ -41,7 +41,7 @@ while True:
         elif (current_gesture == 0 or current_gesture == 2):
             for i in range(0, 180, 1):
                 wrist.angle = i
-                time.sleep(0.02)
+                time.sleep(0.01)
 
         current_gesture = 1
 
@@ -50,10 +50,10 @@ while True:
         if (current_gesture == 0):
             for i in range(0, 90, 1):
                 wrist.angle = i
-                time.sleep(0.02)
+                time.sleep(0.01)
 
         elif (current_gesture == 1):
             for i in range(180, 90, -1):
                 wrist.angle = i
-                time.sleep(0.02)
+                time.sleep(0.01)
         break
