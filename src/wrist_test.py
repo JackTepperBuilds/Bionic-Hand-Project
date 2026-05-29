e0ff79424063152e1c5fee43446ff3dc4317ab7c
Jack Tepper <jatep2005@gmail.com>
2026-05-29 16:04:19 -0400

    fix: changed number inputs for 'Up' and 'Down' as well as .sleep times

--- src/wrist_test.py
+++ src/wrist_test.py
@@ -4,7 +4,7 @@ import time
 from adafruit_motor import servo
 from adafruit_pca9685 import PCA9685
 
-current_gesture = 3
+current_gesture = 2
 
 # Frequency works by sending continuous waves to the servos in this case every 50Hz (20ms). 
 # these continous waves are set to a certain pulse width (1.5ms in a lot of cases) and
@@ -18,7 +18,7 @@ pca.frequency = 50
 # Sets wrist to channel zero and sets its min and max pulses.
 wrist = servo.Servo(pca.channels[0], min_pulse = 500, max_pulse = 2500)
 
-print("0: Up, 1: Down, 3: Exit")
+print("1: Up, 0: Down, 2: Exit")
 
 while True:
     x: int = int(input())
@@ -30,7 +30,7 @@ while True:
         elif (current_gesture == 1):
             for i in range(90, 0, -2):
                 wrist.angle = i
-                time.sleep(0.02)
+                time.sleep(0.01)
 
         current_gesture = 0
 
@@ -41,19 +41,19 @@ while True:
         elif (current_gesture == 0):
             for i in range(90, 180, 2):
                 wrist.angle = i
-                time.sleep(0.02)
+                time.sleep(0.01)
 
         current_gesture = 1
 
     # When ending the program the servo is set back to its center position.
-    elif (x == 3):
+    elif (x == 2):
         if (current_gesture == 0):
             for i in range(0, 90, 2):
                 wrist.angle = 90
-                time.sleep(0.02)
+                time.sleep(0.01)
 
         elif (current_gesture == 1):
             for i in range(180, 90, -2):
                 wrist.angle = i
-                time.sleep(0.02)
+                time.sleep(0.01)
         break
