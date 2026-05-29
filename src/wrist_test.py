2aecb1a3f8561a30a88103ad2accd257b8e0093c
Jack Tepper <jatep2005@gmail.com>
2026-05-29 15:52:56 -0400

    feat: added current_gesture checker for repeated movements

--- src/wrist_test.py
+++ src/wrist_test.py
@@ -4,6 +4,8 @@ import time
 from adafruit_motor import servo
 from adafruit_pca9685 import PCA9685
 
+current_gesture = 3
+
 # Frequency works by sending continuous waves to the servos in this case every 50Hz (20ms). 
 # these continous waves are set to a certain pulse width (1.5ms in a lot of cases) and
 # when code runs to move a servo 180 degrees like 'servo.angle = 180', under the hood
@@ -19,16 +21,40 @@ wrist = servo.Servo(pca.channels[0], min_pulse = 500, max_pulse = 2500)
 print("0: Up, 1: Down, 3: Exit")
 
 while True:
+    global current_gesture
     x: int = int(input())
+
     if (x == 0):
-        for i in range(90, 0, -2):
-            wrist.angle = i
-            time.sleep(0.02)
+        if (current_gesture == 0):
+            continue
+
+        elif (current_gesture == 1):
+            for i in range(90, 0, -2):
+                wrist.angle = i
+                time.sleep(0.02)
+
+        current_gesture = 0
+
     elif (x == 1):
-        for i in range(90, 180, 2):
-            wrist.angle = i
-            time.sleep(0.02)
+        if (current_gesture == 1):
+            continue
+
+        elif (current_gesture == 0):
+            for i in range(90, 180, 2):
+                wrist.angle = i
+                time.sleep(0.02)
+
+        current_gesture = 1
+
     # When ending the program the servo is set back to its center position.
     elif (x == 3):
-        wrist.angle = 90
+        if (current_gesture == 0):
+            for i in range(0, 90, 2):
+                wrist.angle = 90
+                time.sleep(0.02)
+
+        elif (current_gesture == 1):
+            for i in range(180, 90, -2):
+                wrist.angle = i
+                time.sleep(0.02)
         break
