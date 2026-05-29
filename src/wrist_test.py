ff8dcde20420af46b240227ea1c849242bd39b9a
Jack Tepper <jatep2005@gmail.com>
2026-05-29 15:33:13 -0400

    fix: moved input into while loop and changed the frequency to 50

--- src/wrist_test.py
+++ src/wrist_test.py
@@ -4,22 +4,22 @@ import time
 from adafruit_motor import servo
 from adafruit_pca9685 import PCA9685
 
-# Frequency works by sending continuous waves to the servos in this case every 333Hz (3.09ms). 
+# Frequency works by sending continuous waves to the servos in this case every 50Hz (20ms). 
 # these continous waves are set to a certain pulse width (1.5ms in a lot of cases) and
 # when code runs to move a servo 180 degrees like 'servo.angle = 180', under the hood
 # the angle is converted to a pulse width (In my case for DS3225 its 2.0ms). This 
 # becomes the new pulse width that moves the servo.
 i2c: busio.I2C = busio.I2C(board.SCL, board.SDA)
 pca: PCA9685 = PCA9685(i2c)
-pca.frequency = 333
+pca.frequency = 50
 
 # Sets wrist to channel zero and sets its min and max pulses.
 wrist = servo.Servo(pca.channels[0], min_pulse = 500, max_pulse = 2500)
 
 print("0: Up, 1: Down, 3: Exit")
-x: int = int(input())
 
 while True:
+    x: int = int(input())
     if (x == 0):
         for i in range(90, 0, -2):
             wrist.angle = i
