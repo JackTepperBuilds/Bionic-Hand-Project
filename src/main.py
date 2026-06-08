f32b762582a35147b8a2183e1462fd2bb00ce74d
Jack Tepper <jatep2005@gmail.com>
2026-06-08 00:01:33 -0400

    feat: set code into a main function and implimented a main guard

--- src/main.py
+++ src/main.py
@@ -2,12 +2,18 @@ from hand import Hand
 from recognition import Recognize
 import threading
 
-hand = Hand()
-recognizer = Recognize()
+def main():
+    hand = Hand()
+    recognizer = Recognize()
 
-recog_loop = threading.Thread(target = recognizer.recognize).start() # Runs the recognizer paired with the camera.
+    recog_loop = threading.Thread(target = recognizer.recognize)
+    recog_loop.start() # Runs the recognizer paired with the camera.
 
-while True:
-    gesture = recognizer.gesture_str
+    while True:
+        gesture = recognizer.gesture_str
 
-    hand.run_hand(gesture)
+        hand.run_hand(gesture)
+
+# Main guard
+if __name__ == "__main__":
+    main()
