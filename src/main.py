c563f800399adc08f9ee8e8c37ad8cc62c0fc0b8
Jack Tepper <jatep2005@gmail.com>
2026-06-21 01:58:34 -0400

    feat: when keyboard button 'd' is pressed end the main loop while the frame generator ends

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 from hand import Hand
 from recognition import Recognize
+from vision import Vision
 import threading
 
 def main():
     hand = Hand()
     recognizer = Recognize()
+    eyes = Vision()
 
     recog_loop = threading.Thread(target = recognizer.recognize)
     recog_loop.start() # Runs the recognizer paired with the camera.
@@ -13,6 +15,10 @@ def main():
         gesture = recognizer.gesture_str
 
         hand.run_hand(gesture)
+
+        # If end == 1, end the main loop while the vision generator ends.
+        if eyes.end == 1:
+            break
 
 # Main guard
 if __name__ == "__main__":
