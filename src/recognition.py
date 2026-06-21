43227794e0fb88cda0557c5f5cf309ec14050bd0
Jack Tepper <jatep2005@gmail.com>
2026-06-21 01:46:06 -0400

    Fix: AttributeError

--- src/recognition.py
+++ src/recognition.py
@@ -31,7 +31,7 @@ class Recognize:
         else:
             print("Nothing")
 
-        print(result.pose_landmarks[0][0])
+        print(result.hand_landmarks[0][4])
 
     # This method reads the frames from the vision class's generator and uses the built in landmarks for gesture
     # recognition.
