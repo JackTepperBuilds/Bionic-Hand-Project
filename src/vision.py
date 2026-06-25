ac4b8a61f26d17785042f00aba6f63aebceaf594
Jack Tepper <jatep2005@gmail.com>
2026-06-25 00:15:47 -0400

    fix: TypeError, incorrect argument passed to camera.generator

--- src/vision.py
+++ src/vision.py
@@ -5,7 +5,7 @@ from typing import Iterator
 
 class Vision:
     # Constructor initializes the camera, sets the config, passes the config to the camera, then starts the camera.
-    def __init__(self, runner):
+    def __init__(self, runner: bool):
         self.picam2 = Picamera2()
         config = self.picam2.create_preview_configuration({'size': (640, 480), 'format': 'RGB888'}, transform = Transform(hflip = True))
         self.picam2.configure(config)
