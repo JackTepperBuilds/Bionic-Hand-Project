65151a12d8e7cfe686dfd2b55e60d1cfb99f750a
Jack Tepper <jatep2005@gmail.com>
2026-06-27 00:51:18 -0400

    fix: TypeError, unneeded argument.

--- src/vision.py
+++ src/vision.py
@@ -5,7 +5,7 @@ from typing import Iterator
 
 class Vision:
     # Constructor initializes the camera, sets the config, passes the config to the camera, then starts the camera.
-    def __init__(self, runner: bool):
+    def __init__(self):
         self.picam2 = Picamera2()
         config = self.picam2.create_preview_configuration({'size': (640, 480), 'format': 'RGB888'}, transform = Transform(hflip = True))
         self.picam2.configure(config)
@@ -14,8 +14,8 @@ class Vision:
         self.frame = None
         self.end_program = 0
 
-    def generator(self, ) -> Iterator:
+    def generator(self) -> Iterator:
         while True:
             self.frame = self.picam2.capture_array()
-            
+
             yield self.frame
