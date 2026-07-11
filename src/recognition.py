611b8bb8f5a02a9f7ca0f397654afd757283e186
Jack Tepper <jatep2005@gmail.com>
2026-07-11 01:36:36 -0400

    feat: test if larger width and height values significantly reduce performance

--- src/recognition.py
+++ src/recognition.py
@@ -6,8 +6,8 @@ import cv2 as cv
 import time
 import threading
 
-WIDTH = 640
-HEIGHT = 480
+WIDTH = 1080
+HEIGHT = 1920
 class Recognize:
     def __init__(self):
         # Sets references to MediaPipes recognizer classes.
