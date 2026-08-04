078fd4d50622aaedc24027a964ef4c8b8cb38f77
Jack Tepper <jatep2005@gmail.com>
2026-08-04 18:41:24 -0400

    fix: updated control code to test MK1 wrist and finger

--- src/wrist_test.py
+++ src/wrist_test.py
@@ -1,111 +1,103 @@
-import board # GPIO & I2C control
-import busio # Set up I2C communication protocol.
+import board  # GPIO & I2C control
+import busio  # Set up I2C communication protocol.
 import time
 from adafruit_motor import servo
 from adafruit_pca9685 import PCA9685
 
-current_gesture = 4
+current_pointer = 4
 current_wrist = 4
 
-# Frequency works by sending continuous waves to the servos in this case every 50Hz (20ms). 
-# these continous waves are set to a certain pulse width (1.5ms in a lot of cases) and
-# when code runs to move a servo 180 degrees like 'servo.angle = 180', under the hood
-# the angle is converted to a pulse width (In my case for DS3225 its 2.0ms). This 
-# becomes the new pulse width that moves the servo.
 i2c: busio.I2C = busio.I2C(board.SCL, board.SDA)
 pca: PCA9685 = PCA9685(i2c)
 pca.frequency = 50
 
-# Sets wrist to channel zero and sets its min and max pulses.
-wrist = servo.Servo(pca.channels[0], min_pulse = 500, max_pulse = 2500)
-pointer = servo.Servo(pca.channels[1], min_pulse = 500, max_pulse = 2500)
+wrist = servo.Servo(pca.channels[0], min_pulse=500, max_pulse=2500)
+pointer = servo.Servo(pca.channels[1], min_pulse=500, max_pulse=2500)
 
-# To make sure wrist is centered before getting input from user.
-wrist.servo = 90
-pointer.servo = 90
+wrist.angle = 90
+pointer.angle = 90
 
-print("0: Down, 1: Up, 2: FingerR, 3: FingerL, 4: Exit")
+print("0: Wrist Down, 1: Wrist Up, 2: Finger Right, 3: Finger Left, 4: Exit")
 
 while True:
     print("How would you like to move the wrist?")
     inp_wrist: int = int(input())
 
-    print("How would you like to move the pointer finger")
-    pointer: int = int(input())
+    print("How would you like to move the pointer finger?")
+    inp_pointer: int = int(input())
 
-    if (inp_wrist == 0):
-        if (current_wrist == 0):
-            continue
+    if inp_wrist == 0:
+        if current_wrist == 0:
+            pass
 
-        elif (current_wrist == 1):
+        elif current_wrist == 1:
             for i in range(180, 0, -2):
                 wrist.angle = i
                 time.sleep(0.01)
 
-        elif (current_wrist == 2):
+        elif current_wrist in (2, 4):
             for i in range(90, 0, -2):
                 wrist.angle = i
                 time.sleep(0.01)
 
         current_wrist = 0
 
-    elif (inp_wrist == 1):
-        if (current_wrist == 1):
-            continue
+    elif inp_wrist == 1:
+        if current_wrist == 1:
+            pass
 
-        elif (current_wrist == 0):
+        elif current_wrist == 0:
             for i in range(0, 180, 2):
                 wrist.angle = i
                 time.sleep(0.01)
 
-        elif (current_wrist == 2):
+        elif current_wrist in (2, 4):
             for i in range(90, 180, 2):
                 wrist.angle = i
                 time.sleep(0.01)
 
         current_wrist = 1
 
-    elif (pointer == 2):
-        if (current_gesture == 2):
-            continue
+    if inp_pointer == 2:
+        if current_pointer == 2:
+            pass
 
-        elif (current_gesture == 3):
-            for i in range(180, 0, -2):
+        elif current_pointer in (3, 4):
+            for i in range(90, 0, -2):
                 pointer.angle = i
                 time.sleep(0.01)
 
-        current_gesture = 2
+        current_pointer = 2
 
-    elif (pointer == 3):
-        if (current_gesture == 3):
-            continue
+    elif inp_pointer == 3:
+        if current_pointer == 3:
+            pass
 
-        elif (current_gesture == 2):
-            for i in range(0, 180, 2):
+        elif current_pointer in (2, 4):
+            for i in range(90, 180, 2):
                 pointer.angle = i
                 time.sleep(0.01)
 
-        current_gesture = 3
+        current_pointer = 3
 
-    # When ending the program the servo is set back to its center position.
-    elif (inp_wrist == 4 and pointer == 4):
-        if (current_wrist == 0):
+    if inp_wrist == 4 and inp_pointer == 4:
+        if current_wrist == 0:
             for i in range(0, 90, 2):
                 wrist.angle = i
                 time.sleep(0.01)
 
-        if (current_wrist == 1):
+        elif current_wrist == 1:
             for i in range(180, 90, -2):
                 wrist.angle = i
                 time.sleep(0.01)
 
-        if (current_gesture == 2):
-            for i in range(180, 90, -2):
+        if current_pointer == 2:
+            for i in range(0, 90, 2):
                 pointer.angle = i
                 time.sleep(0.01)
 
-        if (current_gesture == 3):
-            for i in range(0, 90, 2):
+        elif current_pointer == 3:
+            for i in range(180, 90, -2):
                 pointer.angle = i
                 time.sleep(0.01)
 
